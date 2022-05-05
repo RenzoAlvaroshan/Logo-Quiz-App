a71bd32be50cc8f651431a92024cda5515fc0a89
m salman a <mohammadalfarisi6@gmail.com>
2022-05-05 22:28:44 +0700

    feat: add Core Data integration and Logo model

---
+++ Logo Quiz App/Model/Logo.swift
@@ -0,0 +1,24 @@
+//
+//  Logo.swift
+//  Logo Quiz App
+//
+//  Created by Mohammad Alfarisi on 05/05/22.
+//
+
+import Foundation
+
+struct Logo {
+    let name: String
+    let imageUrl: String
+    let level: Int
+}
+
+let logos = [
+    Logo(name: "Aqua", imageUrl: "AQUA", level: 0),
+    Logo(name: "Djarum", imageUrl: "DJARUM", level: 0),
+    Logo(name: "Pertamina", imageUrl: "PERTAMINA", level: 0),
+    Logo(name: "PLN", imageUrl: "PLN", level: 0),
+    Logo(name: "SCTV", imageUrl: "SCTV", level: 0),
+    Logo(name: "Telkom", imageUrl: "TELKOM", level: 0),
+    Logo(name: "Tiket.com", imageUrl: "TIKET.COM", level: 0)
+]
