6c96ac987eac853cab92a2247297cf2032d481e6
hnslmp <ancemo14@gmail.com>
2022-05-05 20:42:42 +0700

    feat:basic UI + tableview

---
+++ Logo Quiz App/Controller/SelectLevelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SelectLevelTableViewCell.swift
+//  Logo Quiz App
+//
+//  Created by Hansel Matthew on 04/05/22.
+//
+
+import UIKit
+
+class SelectLevelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Logo Quiz App/Model/LevelModel.swift
@@ -0,0 +1,8 @@
+//
+//  LevelModel.swift
+//  Logo Quiz App
+//
+//  Created by Hansel Matthew on 04/05/22.
+//
+
+import Foundation
